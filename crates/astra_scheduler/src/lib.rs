2cbfaf4b1ba08be4f41d948bf4416be0dc6acda0
Salim <s0urc3.1er@gmail.com>
2025-12-02 16:52:36 +0100

    fixed

--- crates/astra_scheduler/src/lib.rs
+++ crates/astra_scheduler/src/lib.rs
@@ -1,29 +1,39 @@
 use flume::{Receiver, unbounded, Sender};
 
-struct Corpus {
-    normal: Sender<Vec<u8>>,
-    priority: Sender<Vec<u8>>,
-    normal_rx: Receiver<Vec<u8>>,
-    priority_rx: Receiver<Vec<u8>>,
+type Corpus = Vec<Vec<u8>>;
+
+#[derive(Clone)]
+pub struct InputsQueue {
+    send_normal_queue: Sender<Corpus>,
+    send_priority_queue: Sender<Corpus>,
+    recv_normal_queue: Receiver<Corpus>,
+    recv_priority_queue: Receiver<Corpus>,
 }
 
-impl Corpus {
-    fn new() -> Self {
-        let (normal, normal_rx) = unbounded();
-        let (priority, priority_rx) = unbounded();
-        Self { normal, priority, normal_rx, priority_rx }
+impl InputsQueue {
+    pub fn new() -> Self {
+
+        let (send_normal_queue, recv_normal_queue) = unbounded();
+        let (send_priority_queue, recv_priority_queue) = unbounded();
+
+        Self { 
+            send_normal_queue,
+            send_priority_queue,
+            recv_normal_queue,
+            recv_priority_queue 
+        }
     }
     
-    fn get_next(&self) -> Option<Vec<u8>> {
-        self.priority_rx.try_recv().ok()
-            .or_else(|| self.normal_rx.try_recv().ok())
+    pub fn get_next(&self) -> Option<Corpus> {
+        self.recv_priority_queue.try_recv().ok()
+            .or_else(|| self.recv_normal_queue.try_recv().ok())
     }
     
-    fn add_normal(&self, input: Vec<u8>) {
-        let _ = self.normal.send(input);
+    pub fn add_normal(&self, Corpus: Corpus) {
+        let _ = self.send_normal_queue.send(Corpus);
     }
     
-    fn add_priority(&self, input: Vec<u8>) {
-        let _ = self.priority.send(input);
+    pub fn add_priority(&self, Corpus: Corpus) {
+        let _ = self.send_priority_queue.send(Corpus);
     }
 }
